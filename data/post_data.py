6469adb413542565c4b8c6b72bcb07e0a6d5af5f
Wyatt1026 <ww275@sussex.ac.uk>
2023-05-19 19:07:01 +0800

    更换了新的API

--- data/post_data.py
+++ data/post_data.py
@@ -32,6 +32,7 @@ class PostData(Enum):
     video_list_headers = {
         'authority': 'api.bilibili.com',
         'method': 'GET',
+        'path':'',
         'scheme':'https',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-language': 'zh-CN,zh;q=0.9',
