eb434d2e4912003284e3a4f1118edf5b9d6eb51d
好名字 <52040067+sunny7010@users.noreply.github.com>
2023-05-25 21:41:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ from core.bilibili import Bilibili
 if __name__ == '__main__':
     bilibili = Bilibili()
     bilibili.go()
+    
+def main_handler(*args):  # 腾讯云函数
+    bilibili = Bilibili()
+    bilibili.go()
