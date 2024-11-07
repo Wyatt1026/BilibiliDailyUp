9eeb08ef74891824697b5b021bd96cd7ccc90381
litscorpi <79298405+litscorpi-star@users.noreply.github.com>
2024-11-07 15:49:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ Project Address: https://github.com/Wyatt1026/BilibiliDailyUp
 
 from core.bilibili import Bilibili
 from config import config
-from os import environ
+import os
 
 if __name__ == '__main__':
-    if not config.USE_ENVIRONMENT_VARIABLE:
-        ck_list = config.COOKIE_LIST
+    cookies = os.environ.get('COOKIES')
+    ck_list = cookies.split(",")
+    
         for ck in ck_list:
             bilibili = Bilibili(ck)
             bilibili.go()
-    else:
-        ck = environ.get('BILIBILI')
-        if not ck:
-            print("未设置BILIBILI这个环境变量 任务终止")
-        else:
-            bilibili = Bilibili(ck)
-            bilibili.go()
+        
+    
