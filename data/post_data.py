0f1235c9192e9e303647cfa3cb28e2d6bb9296c7
litscorpi <79298405+litscorpi-star@users.noreply.github.com>
2024-11-23 10:30:25 +0800

    Update post_data.py

--- data/post_data.py
+++ data/post_data.py
@@ -34,8 +34,9 @@ class PostData(Enum):
 
     }
     comics_sign_header = {
-        "Cookie": '',
-        "Content-Type": "application/x-www-form-urlencoded"
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': '',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
     }
 
     video_list_headers = {
