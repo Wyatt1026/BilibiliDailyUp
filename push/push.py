e431078d7a96bc75a59748de37a6a97699484823
Wyatt1026 <ww275@sussex.ac.uk>
2025-03-24 12:55:38 +0800

    fix..

--- push/push.py
+++ push/push.py
@@ -6,23 +6,6 @@ import requests
 import json
 
 
-def pushplus_push(token, content, title='Bilibili助手提醒', template='markdown'):
-    url = 'http://www.pushplus.plus/send'
-    data = {
-        "token": token,
-        "title": title,
-        "content": content,
-        "template": template
-    }
-    body = json.dumps(data).encode(encoding='utf-8')
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=body, headers=headers)
-    print(response.text)
-    if response.status_code == 200:
-        return 1
-    return None
-
-
 def sever_push(msg, token):
     data = {
         "title": "Bilibili助手提醒",
